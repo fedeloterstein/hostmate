337cb63b1f2cc36c9926e112ce6967ce0b9d40c0
fedeloterstein <federicoloterstein@gmail.com>
2023-04-18 00:27:27 -0300

    chore: change avatar modal

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,20 @@
 import { onLogout } from '@/api/AuthAPI';
 import { Logo } from '@/assets/icons/Logo';
 import { auth } from '@/firebase.config';
-import { Avatar, Badge, Box, Button, HStack, Stack, Text } from '@chakra-ui/react';
+import {
+  Avatar,
+  Badge,
+  Box,
+  Button,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useSession, signOut } from 'next-auth/react';
 import Link from 'next/link';
@@ -12,7 +25,7 @@ export const Navbar = () => {
   const router = useRouter();
   const [session, setsession] = useState<any>();
   const [loading, setloading] = useState(true);
-console.log(session);
+  console.log(session);
 
   useEffect(() => {
     onAuthStateChanged(auth, (res: any) => {
@@ -27,19 +40,35 @@ console.log(session);
   }, []);
 
   return (
-    <Stack w={'100%'} justify={'space-between'} p={'37px'}    direction={['column', 'column', 'row']}>
+    <Stack w={'100%'} justify={'space-between'} p={'37px'} direction={['column', 'column', 'row']}>
       <Link href={'/'}>
         <HStack>
-        <Logo />
-        <Badge colorScheme='purple'>Beta v1</Badge>
+          <Logo />
+          <Badge colorScheme="purple">Beta v1</Badge>
         </HStack>
       </Link>
       {session?.accessToken && (
-        <HStack>
-          <Text>{session.email}</Text>
-          <Avatar size={'sm'} onClick={onLogout} />
-        </HStack>
+        <Menu>
+          <MenuButton as={Button} rounded={'full'} variant={'link'} cursor={'pointer'} minW={0}>
+            <Avatar
+              size={'sm'}
+              src={
+                'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+              }
+            />
+          </MenuButton>
+          <MenuList>
+            <MenuItem>{session.email}</MenuItem>
+            <MenuDivider />
+            <MenuItem>Profile</MenuItem>
+            <MenuItem>Interviews</MenuItem>
+            <MenuDivider />
+            <MenuItem  onClick={onLogout} >Logout</MenuItem>
+          </MenuList>
+        </Menu>
       )}
     </Stack>
   );
 };
+
+
