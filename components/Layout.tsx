d6d92f61d5dbd7275a9ec8ea70186428f7fb33d2
fedeloterstein <federicoloterstein@gmail.com>
2023-04-26 21:28:04 -0300

    feat: analytics

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,14 +13,14 @@ export const Layout = ({ children }: { children: ReactNode }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-VV3G6ZNQZY" />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Z4QT5BM9QX" />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
         
-          gtag('config', 'G-VV3G6ZNQZY');
+          gtag('config', 'G-Z4QT5BM9QX');
         `}
       </Script>
       <Container maxW="container.xl">{children}</Container>
